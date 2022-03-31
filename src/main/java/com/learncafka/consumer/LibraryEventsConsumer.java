e0a6f8fdb01f67e15b868ae564494da304974880
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-03-31 21:47:17 +0200

    14.60-61 Build Service Layer to process LibraryEvent - Add-Modify

--- src/main/java/com/learncafka/consumer/LibraryEventsConsumer.java
+++ src/main/java/com/learncafka/consumer/LibraryEventsConsumer.java
@@ -1,7 +1,10 @@
 package com.learncafka.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learncafka.service.LibraryEventService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +12,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LibraryEventsConsumer {
 
+    @Autowired
+    private LibraryEventService libraryEventService;
+
     @KafkaListener(topics = {"library-events"})
-    public void onMessage(ConsumerRecord<Integer,String> consumerRecord){
+    public void onMessage(ConsumerRecord<Integer,String> consumerRecord) throws JsonProcessingException {
         log.info("ConsumerRecord : {} ", consumerRecord);
+        libraryEventService.processLibraryEvent(consumerRecord);
     }
 }
