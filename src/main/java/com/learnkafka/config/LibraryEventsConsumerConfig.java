d12bd44327235770d26a1e5869e2ae9d519fbe07
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-02 11:45:39 +0200

    17.66-67 Custom Error Handler - Retry in Kafka Consumer

--- src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/config/LibraryEventsConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.learnkafka.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,9 +8,15 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.BackOffPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @EnableKafka
+@Slf4j
 public class LibraryEventsConsumerConfig {
 
     @Bean
@@ -21,8 +28,31 @@ public class LibraryEventsConsumerConfig {
         //change offset, in this case from default to manual
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         //multiple tracks of one kafka listener
-        factory.setConcurrency(3);
+        //factory.setConcurrency(3);
+        factory.setErrorHandler((thrownException, data) -> {
+            log.info("Exception in consumerConfig is {}, and the record is {} ", thrownException.getMessage(), data);
+        });
+        factory.setRetryTemplate(retryTemplate());
         return factory;
+    }
+
+    private RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy());
+        return retryTemplate;
+    }
+
+    private BackOffPolicy fixedBackOffPolicy() {
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+        return fixedBackOffPolicy;
+    }
+
+    private RetryPolicy simpleRetryPolicy() {
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+        return simpleRetryPolicy;
     }
 
 
